9f7d48a828ab5c6c60013bd496d9f80bf7dbde70
akhilcjacob <akhilcjacob@gmail.com>
2017-12-08 19:04:45 -0500

    added some more files

---
+++ flask-boilerplate/__init__.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='flask-boilerplate',
+    packages=['flask-boilerplate'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
